7e8c82937f8745486c3d53d9d42d023cdf3bf501
Rafal Mi <rafal.m4@gmail.com>
2026-03-11 01:05:33 +0100

    Simplified animation 14

--- composeApp/src/commonMain/kotlin/org/pinczow/animations/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/pinczow/animations/Constants.kt
@@ -2,7 +2,7 @@ package org.pinczow.animations
 
 class Constants {
     companion object {
-        const val duration = 3000
+        const val duration = 1000
     }
 }
 
--- composeApp/src/commonMain/kotlin/org/pinczow/animations/animations/Animation14.kt
+++ composeApp/src/commonMain/kotlin/org/pinczow/animations/animations/Animation14.kt
@@ -11,12 +11,14 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.LinearProgressIndicator
@@ -95,6 +97,19 @@ fun Animation14() {
     val buttonWidth = animateDpAsState(
         targetValue = if(expanded) {
             100.dp
+
+        } else {
+            8.dp
+        },
+        label = "width",
+        animationSpec = tween(
+            durationMillis = duration,
+            easing = FastOutLinearInEasing,
+        )
+    )
+    val buttonWHeight = animateDpAsState(
+        targetValue = if(expanded) {
+            ButtonDefaults.MinHeight
         } else {
             8.dp
         },
@@ -164,7 +179,7 @@ fun Animation14() {
             ) {
                 LinearProgressIndicator(
                     modifier = Modifier
-                        .weight(1f, fill = true)
+                        .weight(4f, fill = true)
                         .padding(start = 8.dp, end = 0.dp)
                     ,
                     progress = { 0.75f }
@@ -172,16 +187,24 @@ fun Animation14() {
                 Button(
                     modifier = Modifier
                         .width(buttonWidth.value)
-                        .height(40.dp)
+                        .height(buttonWHeight.value)
                         .padding(horizontal = 8.dp)
-                        .graphicsLayer {
-                            scaleX = buttonScaleXTween
-                            scaleY = buttonScaleYTween
-                        }
+//                        .graphicsLayer {
+//                            scaleX = buttonScaleXTween
+//                            scaleY = buttonScaleYTween
+//                        }
                     ,
                     onClick = {}
                 ) {
-                    Text("Click")
+                    Text(
+                        modifier = Modifier
+                            .graphicsLayer {
+                            scaleX = buttonScaleXTween
+                            scaleY = buttonScaleYTween
+                        }
+                        ,
+                        text = "Click"
+                    )
                 }
             }
         }
